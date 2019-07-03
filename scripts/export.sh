087ca751a4c178ccbaa28c3907ee71f4d50f2f9e
Akshata <akshata@Akshatas-Air.fios-router.home>
2019-07-02 23:31:06 -0400

    snowflake_export

---
+++ scripts/export.sh
@@ -0,0 +1,1 @@
+nowsql -c example -d SNOW_AKSHATA_DB -w SNOW_AKSHATA_WH -f /Users/akshata/snowflake-tutorials/scripts/snowflakefinal.sql -o output_format=csv -o header=false -o timing=false -o friendly=false  > /Users/akshata/Desktop/output_file.csv
